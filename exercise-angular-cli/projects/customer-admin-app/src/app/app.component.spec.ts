96fcf03b08f4f48bef984d422553e12da5b96296
MichaelEgli <michaelegli1879@gmail.com>
2022-03-21 14:42:50 +0100

    angular cli until 10 done

--- exercise-angular-cli/projects/customer-admin-app/src/app/app.component.spec.ts
+++ exercise-angular-cli/projects/customer-admin-app/src/app/app.component.spec.ts
@@ -19,14 +19,14 @@ describe('AppComponent', () => {
   it(`should have as title 'customer-admin-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('customer-admin-app 1');
+    expect(app.title).toEqual('customer-admin-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
+    expect(compiled.querySelector('h1').textContent).toContain(
       'customer-admin-app app is running!'
     );
   });
